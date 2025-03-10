a0f43cd981cad16180289a5fb2b27cd42516ac4e
HamintonBlandon <160552739+HamintonBlandon@users.noreply.github.com>
2025-03-10 00:07:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,17 +3,37 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import morgan from "morgan";
 import usersRouter from "./routers/users/users-router.js";
-import productosRouter from "./routers/productos/productos-router.js";
+import productsRouter from "./routers/products/products-router.js";
+import { Database } from "./database/db.js";
+import {rateLimitMiddleware} from "./middlewares/rateLimit.js"
+import http from "http";
+import { SocketHandler } from "./sockets/socket.js";
 
 const app = express();
+const database = new Database();
+const server = http.createServer(app);
+
+const socketIo = new SocketHandler(server);
+app.set("socketio", socketIo.iosocket);
+
+
+database.setup();
+
+app.use(express.json());  // Para manejar JSON
+app.use(express.urlencoded({ extended: true }));  // Para manejar datos en formularios
 
 app.use(cors());
-app.use(morgan());
+app.use(morgan("dev"));
 app.use(bodyParser());
-
+app.use(rateLimitMiddleware);
 app.use("/users", usersRouter);
-app.use("/productos", productosRouter)
+app.use("/products", productsRouter);
 
-app.listen(8000, () => {
-    console.log("App running on port 8000");
+new SocketHandler(server);
+
+server.listen(8080, () => {
+    console.log("App running on port 8080");
 });
+
+//este error es porque no tengo bien configurado valkey en el docker, debo encender docker para que me conecte y funcione; si no lo enciendo no pasara nada
+
